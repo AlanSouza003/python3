91e13870b0b663ce52399aa557fc0b3392cddcab
Alan Souza <alansouzagit@outlook.com>
2026-03-14 11:16:42 -0300

    Update ex39.py

--- hello_world/mundo_2/aula12/python_exercicio/ex039.py
+++ hello_world/mundo_2/aula12/python_exercicio/ex039.py
@@ -1,15 +1,15 @@
 from datetime import date
 cor = {'limpa':'\033[0m', 'vermelho':'\033[1;91m', 'verde':'\033[1;92m',
-       'amarelo':'\033[1;93m', 'branco':'\033[1;97m','azul':'\033[1;95m'}
+       'amarelo':'\033[1;93m', 'branco':'\033[1;97m','azul':'\033[1;94m'}
 # Titulo
-print(f'-'*30)
-print(f'{'ALISTAMENTO MILITAR 🪖':^29}')
-print(f'-'*30)
-ano = int(input(f'Digite o ano em que nasceu: '))
+print(f'{cor['branco']}-{cor['limpa']}'*30)
+print(f'{cor['azul']}{'ALISTAMENTO MILITAR 🪖':^29}{cor['limpa']}')
+print(f'{cor['branco']}-{cor['limpa']}'*30)
+ano = int(input(f'{cor['branco']}Digite o ano em que nasceu: {cor['limpa']}'))
 ano_atual = date.today().year
 if ano_atual - ano == 18:
-    print(f'Esta na hora de se ALISTAR')
+    print(f'{cor['amarelo']}ESTA NA HORA DE SE ALISTAR{cor['limpa']}')
 elif ano_atual - ano > 18:
-    print(f'Passou da hora de se ALISTAR')
+    print(f'{cor['vermelho']}PASSOU DA HORA DE SE ALISTAR{cor['limpa']}')
 else:
-    print(f'Ainda vai se ALISTAR')
+    print(f'{cor['verde']}AINDA VAI SE ALISTAR{cor['limpa']}')
